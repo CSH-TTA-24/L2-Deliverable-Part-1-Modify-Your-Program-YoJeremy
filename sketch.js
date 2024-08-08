efe2e84d4f0fd451e7c15e8a4e6096cd575309e7
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 09:50:10 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+  background(0, 0, 0);
+}
+
+function draw() {
+  
+}
+
+function mouseClicked() {
+  let r = random(0, 255);
+  let g = random(0, 255);
+  let b = random(0, 255);
+  fill(r, g, b);
+let diameter = random(25,100)
+  ellipse(mouseX, mouseY, diameter, diameter);
+}
